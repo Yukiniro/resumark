5c67534a5d5443db131fffedab1d05134edc96d6
Yukiniro <yukiniro@hotmail.com>
2022-04-03 10:45:46 +0800

    feat: add sugar-high.css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import md from "./intro.md";
 import { highlight } from 'sugar-high';
 import { marked } from "marked";
 import './index.css'
+import './sugar-high.css'
 
 const root = document.getElementById("app");
 marked.setOptions({
