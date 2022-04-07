8995bbf73d68b5cb3fdbf1604c0f259be8bafa88
Yukiniro <yukiniro@hotmail.com>
2022-04-07 22:22:39 +0800

    feat: support avatarShape

--- plugin/vite-plugin-resume.ts
+++ plugin/vite-plugin-resume.ts
@@ -24,20 +24,25 @@ function parseBlock(block: Block) {
   const { config, content } = block;
   const nextContent = [...content];
   let html = marked.parse(nextContent.join("\n"));
-  const { blockClassName, blockStyle, name, summary, avatarUrl } = config;
+  const { blockClassName, blockStyle, name, summary, avatarUrl, avatarShape } =
+    config;
   let style = createStyleTextFromConfig(blockStyle);
   let className = classNames("re__block", blockClassName);
   if (isUndefined(name)) {
     html = `<div style="${style}" class="${className}">${html}</div>`;
   } else {
     className = classNames("re__personal", className);
+    let avatarClassName = classNames(
+      "re__avatar",
+      avatarShape === "circle" && "re__avatar--circle"
+    );
     html = `
         <div class="${className}">
           <div class="re__personal__info">
             <h1>${name}</h1>
             <span>${summary}</span>
           </div>
-          <img class="re__avatar" src=${avatarUrl} />
+          <img class="${avatarClassName}" src=${avatarUrl} />
         </div>
       `.trim();
   }
