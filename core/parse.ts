0847db48bbbfcd084dee55889fce8b90a64be6fe
Yukiniro <yukiniro@hotmail.com>
2022-04-08 22:19:38 +0800

    feat: parse.ts update type

--- core/parse.ts
+++ core/parse.ts
@@ -1,7 +1,7 @@
 import yaml from "js-yaml";
 import { marked } from "marked";
 import { MainBlockConfig } from "./types";
-import { isUndefined, isObject } from "bittydash";
+import { isUndefined } from "bittydash";
 import classNames from "classnames";
 
 interface Block {
@@ -9,12 +9,14 @@ interface Block {
   content: Array<string>;
 }
 
-function createStyleTextFromConfig(blockStyle: object | undefined) {
-  return isObject(blockStyle)
-    ? Object.keys(blockStyle)
-        .map((key) => `${key}:${blockStyle[key]}`)
-        .join(";")
-    : "";
+interface BlockStyle {
+  [key: string]: string | number;
+}
+
+function createStyleTextFromConfig(blockStyle: BlockStyle = {}) {
+  return Object.keys(blockStyle).map((key) => {
+    return `${key}:${blockStyle[key]}`;
+  });
 }
 
 function parseBlock(block: Block) {
@@ -23,7 +25,7 @@ function parseBlock(block: Block) {
   let html = marked.parse(nextContent.join("\n"));
   const { blockClassName, blockStyle, name, summary, avatarUrl, avatarShape } =
     config;
-  let style = createStyleTextFromConfig(blockStyle);
+  let style = createStyleTextFromConfig((blockStyle || {}) as BlockStyle);
   let className = classNames("re__block", blockClassName);
   if (isUndefined(name)) {
     html = `<div style="${style}" class="${className}">${html}</div>`;
@@ -50,9 +52,9 @@ function parseBlock(block: Block) {
 function parse(markdownText: string) {
   const lines = markdownText.trim().split(/\r?\n/g);
   let inCofnigTag = false;
-  let curConfigArr = [];
-  let curContentArr = [];
-  let blocks = [];
+  let curConfigArr: Array<string> = [];
+  let curContentArr: Array<string> = [];
+  let blocks: Array<Block> = [];
 
   const append = () => {
     if (curContentArr.length) {
