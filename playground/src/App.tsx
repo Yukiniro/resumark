2aac102c7ec79a26829f7531660c804646fc9f81
Yukiniro <yukiniro@hotmail.com>
2022-04-09 14:08:52 +0800

    feat：add some buttons

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,13 +1,29 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { parse } from "../../core/parse";
+import pdf from "html-pdf";
 import md from "../../src/intro.md";
 import "../../src/default.css";
 
 function App() {
-  const [markdown, setMarkdown] = useState(md.trim());
+  const initValueRef = useRef(md.trim());
+  const [markdown, setMarkdown] = useState(initValueRef.current);
   const [html, setHTML] = useState("");
   const onTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMarkdown(e.target.value);
+  };
+
+  const resetOnClick = () => {
+    setMarkdown(initValueRef.current);
+  };
+  const exportOnClick = () => {
+    console.log('export');
+    pdf.create(html).toFile("./resume.pdf", function (err: ErrorEvent) {
+      if (err) {
+        alert("Export Fail");
+      } else {
+        alert("Export Success");
+      }
+    });
   };
 
   useEffect(() => {
@@ -24,6 +40,14 @@ function App() {
       <h1 className="text-center leading-loose font-mono text-xl p-8">
         Preview Your Resume With Markdown
       </h1>
+      <div className="flex justify-center py-4">
+        <button onClick={resetOnClick} className="btn mx-2">
+          Reset
+        </button>
+        <button onClick={exportOnClick} className="btn mx-2">
+          Export
+        </button>
+      </div>
       <div className="flex min-h-screen-sm">
         <textarea
           className="w-1/2 border-solid border-1 border-cyan-200"
---
+++ playground/windi.config.js
@@ -0,0 +1,5 @@
+export default {
+  shortcuts: {
+    btn: "py-2 px-4 font-mono rounded-lg shadow-md text-white bg-blue-400 hover:bg-blue-500 transition-all",
+  },
+};
